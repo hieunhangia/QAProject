86339792d8ed23ed1af2bb29c93275f28babeb63
hieunhangia <hieuvippro531@gmail.com>
2026-02-02 21:30:49 +0700

    Add QuestionAppService and IQuestionAppService for question management

--- src/QAProject.Domain.Shared/Constants/Roles.cs
+++ src/QAProject.Domain.Shared/Constants/Roles.cs
@@ -2,7 +2,7 @@ namespace QAProject.Constants;
 
 public class Roles
 {
-    public const string Admin = "Admin";
+    public const string Admin = "admin";
     public const string BA = "BA";
     public const string User = "User";
 }
